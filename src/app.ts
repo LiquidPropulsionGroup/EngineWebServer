094f1139ce10c08c84d7f163bffaa7d7a60d6830
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2021-01-05 09:57:49 -0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import * as bodyParser from "body-parser";
 import { TestRoutes } from "./routes/test_routes";
+import { Serial } from "./routes/serial";
 
 class App {
   public app: express.Application;
   private test_routes: TestRoutes = new TestRoutes();
+  private serial: Serial = new Serial();
 
   constructor() {
     this.app = express();
     this.config();
     this.test_routes.route(this.app);
+    this.serial.route(this.app);
   }
+
   private config(): void {
     // support application/json type post data
     this.app.use(bodyParser.json());

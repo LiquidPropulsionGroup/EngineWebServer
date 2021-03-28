0f86dfcd11825d26aa0295ab2a7da0db8ee4c75e
Jesus Cuevas <32606020+jesuscuevas@users.noreply.github.com>
2021-03-27 22:04:17 -0700

    Update startCache.ts (#29)

--- Caching Database Service/src/routes/startCache.ts
+++ Caching Database Service/src/routes/startCache.ts
@@ -1,5 +1,6 @@
 // Importing dependencies
 import { Application, Request, Response } from "express";
+import { spawn } from "child_process";
 
 // Creating class that initializes the SerialTest route. This will only
 // be used in development.
@@ -12,7 +13,11 @@ export class startCache {
       let action = req.params.action || req.param("action");
 
       // Script code for starting tread goes here
-
+      let pythonProcess = spawn("python", ["-u", "script.py"], {detached: true}); // starts running script.py (a temporary file) in the background
+      pythonProcess.stdout.on("data", (data) => {}); // What to do when stdout is printed (data)
+      pythonProcess.on("exit", (code) => {}); // What to do when program is done
+      let terminateProcess = spawn("bash", ["termscript.sh"], {detached: true}); // ps aux | grep "python -u script.py" | tr -s' ' | cut -d' ' -f2 | xargs kill
+      
       // Returning 404 since only ON and OFF URI exsist
       return res.status(404).send("Action: " + action);
     });

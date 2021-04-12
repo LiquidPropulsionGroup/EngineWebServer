99cad4e073bc3c52bf9fa689bf9602fd410aef49
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2021-04-12 13:59:16 -0700

    Update termscript.sh

--- Caching Database Service/src/routes/termscript.sh
+++ Caching Database Service/src/routes/termscript.sh
@@ -1,2 +1,2 @@
 # Terminates the python script
-ps aux | grep "python -u script.py" | tr --squeeze-repeats ' ' | cut --delimiter=' ' --fields=2 | xargs kill
+ps -a | grep "python" | tr --squeeze-repeats ' ' | cut --delimiter=' ' --fields=1 | xargs kill -9

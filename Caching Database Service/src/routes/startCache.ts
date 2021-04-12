f9e373307276bae60a8e9f48e4031e8e8d9730fa
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2021-04-11 22:08:22 -0700

    updated route

--- Caching Database Service/src/routes/startCache.ts
+++ Caching Database Service/src/routes/startCache.ts
@@ -12,14 +12,25 @@ export class startCache {
       // Grabbing params from the URI
       let action = req.params.action || req.param("action");
 
-      // Script code for starting tread goes here
-      let pythonProcess = spawn("python", ["-u", "script.py"], {detached: true}); // starts running script.py (a temporary file) in the background
-      pythonProcess.stdout.on("data", (data) => {}); // What to do when stdout is printed (data)
-      pythonProcess.on("exit", (code) => {}); // What to do when program is done
-      let terminateProcess = spawn("bash", ["termscript.sh"], {detached: true}); // ps aux | grep "python -u script.py" | tr -s' ' | cut -d' ' -f2 | xargs kill
+      switch (action) {
+        case 'ON':
+          let pythonProcess = spawn("python", ["-u", "script.py"], {detached: true}); // starts running script.py (a temporary file) in the background
+          pythonProcess.stdout.on("data", (data) => {
+            return res.status(500).send(`Python script started:\n${data}`);
+          });
       
-      // Returning 404 since only ON and OFF URI exsist
-      return res.status(404).send("Action: " + action);
+        case 'OFF':
+          let terminateProcess = spawn("bash", ["termscript.sh"], {detached: true}); // ps aux | grep "python -u script.py" | tr -s' ' | cut -d' ' -f2 | xargs kill
+          terminateProcess.on("exit", (code) => {
+            return res.status(500).send(`Python script stopped:\n${code}`);
+          });
+
+        default:
+          // Returning 404 since only ON and OFF URI exsist
+          return res.status(404).send("Action: " + action);
+      }
+      // pythonProcess.stdout.on("data", (data) => {}); // What to do when stdout is printed (data)
+      // pythonProcess.on("exit", (code) => {}); // What to do when program is done
     });
   }
 }
